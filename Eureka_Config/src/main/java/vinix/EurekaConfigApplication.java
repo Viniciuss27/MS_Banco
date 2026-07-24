395598a451babb52b9378d4162ac4e6a01abd78c
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-07-23 22:50:24 -0300

    Configuração finalizada

--- Eureka_Config/src/main/java/vinix/EurekaConfigApplication.java
+++ Eureka_Config/src/main/java/vinix/EurekaConfigApplication.java
@@ -1,13 +1,33 @@
 package vinix;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableConfigServer
+@EnableEurekaServer
 @SpringBootApplication
-public class EurekaConfigApplication {
+public class EurekaConfigApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaConfigApplication.class, args);
 	}
+	
+	@Value("${GIT_PASSWORD}")
+	private String password;
+	
+	@Value("${GIT_USERNAME}")
+	private String username;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("PASSWORD: " + password);
+		System.out.println("USERNAME: " + username);
+	}
+	
+	
 
 }
