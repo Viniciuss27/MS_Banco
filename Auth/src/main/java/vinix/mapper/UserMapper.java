1be4cba449c638c47e6cdd3eaade503288fcd09e
vinicius <viniciusdejjeuspessoal@gmail.com>
2026-07-27 21:07:11 -0300

    Auth: Mapper finalizada

---
+++ Auth/src/main/java/vinix/mapper/UserMapper.java
@@ -0,0 +1,32 @@
+package vinix.mapper;
+
+import java.util.List;
+import java.util.Set;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
+
+import vinix.dto.request.RegisterRequestDTO;
+import vinix.dto.response.UserResponseDTO;
+import vinix.entities.Role;
+import vinix.entities.User;
+
+@Mapper(componentModel = "spring")
+public interface UserMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "roles", ignore = true)
+    User toEntity(RegisterRequestDTO dto);
+
+    @Mapping(target = "roles", source = "roles", qualifiedByName = "rolesToNames")
+    UserResponseDTO toResponseDTO(User user);
+
+    @Named("rolesToNames")
+    default List<String> rolesToNames(Set<Role> roles) {
+        return roles.stream()
+                .map(Role::getRoleName)
+                .toList();
+    }
+}
